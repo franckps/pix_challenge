f4703a3b42819c15308444adf16260597eb4f72a
Francisco Pereira <fran.p.s@hotmail.com>
2021-06-14 19:42:09 -0300

    feat: first commit

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
